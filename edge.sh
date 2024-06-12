74047676f151c26964addf97e3abaf3eeb4dda4a
Kautsar0907 <kautsar0907@gmail.com>
2024-06-12 14:54:11 +0700

    edge.sh
    Edge fixxer 📏
    
    Still facing edge touch issues on miui 13 a12/fog? 
    
    Use this, work for root and no root (universal) 
    
    reboot=uninstall
    
    quotes today: don't buy fog or use fog 🌁

---
+++ edge.sh
@@ -0,0 +1,6 @@
+#Fixxer for fog (redmi 10c) or miui 13 a12
+#Support no root and root devices
+settings put system edge_pressure 0
+settings put system edge_size 0
+settings put system edge_type 0
+setprop debug.input.normalizetouch true
