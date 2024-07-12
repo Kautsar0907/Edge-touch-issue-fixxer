46c65316e12176fc3d4e5ad1a2dd74e0b43b2bdb
eraselk <eraselk@proton.me>
2024-07-12 18:54:51 +0800

    Fix some stuff

--- edge.sh
+++ edge.sh
@@ -2,18 +2,36 @@
 # Support no root and root devices
 
 if ! [ $(id -u) -ne 2000 ] || [ $(id -u) -ne 0 ]; then
-echo "[ERROR] Please run this script using ADB or Root!"
-exit 1
+    echo "[ERROR] Please run this script using ADB or Root!"
+    exit 1
 fi
 
 a=0
 b=0
-(
-settings put system edge_pressure 0 && ((a + 1)) || ((b + 1))
-settings put system edge_size 0 && ((a + 1)) || ((b + 1))
-settings put system edge_type 0 && ((a + 1)) || ((b + 1))
-setprop debug.input.normalizetouch true && ((a + 1)) || ((b + 1))
-) >/dev/null 2>&1
+
+if settings put system edge_pressure 0; then
+    let a++
+else
+    let b++
+fi
+
+if settings put system edge_size 0; then
+    let a++
+else
+    let b++
+fi
+
+if settings put system edge_type 0; then
+    let a++
+else
+    let b++
+fi
+
+if setprop debug.input.normalizetouch true; then
+    let a++
+else
+    let b++
+fi
 
 echo "Success to apply: $a"
 echo "Failed to apply: $b"
