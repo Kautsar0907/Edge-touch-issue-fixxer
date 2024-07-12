478aeaf3afc1964a1f3e45af64585bb2ddec1b6e
eraselk <eraselk@proton.me>
2024-07-12 18:36:49 +0800

    Improve edge.sh

--- edge.sh
+++ edge.sh
@@ -1,6 +1,20 @@
-#Fixxer for fog (redmi 10c) or miui 13 a12
-#Support no root and root devices
-settings put system edge_pressure 0
-settings put system edge_size 0
-settings put system edge_type 0
-setprop debug.input.normalizetouch true
+# Fixer for fog (redmi 10c) or miui 13 a12
+# Support no root and root devices
+
+if ! [ $(id -u) -ne 2000 ] || [ $(id -u) -ne 0 ]; then
+echo "[ERROR] Please run this script using ADB or Root!"
+exit 1
+fi
+
+a=0
+b=0
+(
+settings put system edge_pressure 0 && ((a + 1)) || ((b + 1))
+settings put system edge_size 0 && ((a + 1)) || ((b + 1))
+settings put system edge_type 0 && ((a + 1)) || ((b + 1))
+setprop debug.input.normalizetouch true && ((a + 1)) || ((b + 1))
+) >/dev/null 2>&1
+
+echo "Success to apply: $a"
+echo "Failed to apply: $b"
+true
